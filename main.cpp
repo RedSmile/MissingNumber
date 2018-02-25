b17f37f61beaf988b693a82fb98490833d21ad2f
RedSmile <34597036+RedSmile@users.noreply.github.com>
2018-02-25 14:35:11 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main() {
+	int s[]{0,3,1};
+	const int sizea = (sizeof(s) / 4) + 1;
+	int sa[sizea]{};
+	int a = 0, zero = 0, num = 0;
+
+	for (int j = 0; j < sizea; j++) {
+		for (int i = 0; i < sizea; i++)
+			if (s[i] == a) sa[j] = s[i];
+		if (sa[j] == 0) 
+			zero++;
+		if (zero == 2) {
+			num = j;
+			break;
+		}
+		a++;
+	}
+	std::cout << num << std::endl;
+
+	system("pause");
+	return 0;
+}
